d4e187bdd1d1ad9c334d5bf7a9c14d86c3b43fa7
Alex190901 <purceaaleaxandrugabriel1909@yahoo.com>
2018-11-29 12:54:21 +0200

    Fixed Bugs

--- src/App.js
+++ src/App.js
@@ -6,15 +6,22 @@ import { Projects } from './projects/Projects';
 import { Contact } from './contact/Contact';
 import { Footer } from './footer/Footer';
 
-let scrollY;
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollY: 0,
+      loading: true
+    }
+  }
   handleScroll() {
+    var scrollY = this.state.scrollY;
     if (scrollY > window.pageYOffset) {
       document.getElementById('navigation-bar').style.marginTop = '-0';
     } else {
       document.getElementById('navigation-bar').style.marginTop = '-60px';
     }
-    scrollY = window.pageYOffset;
+    this.setState({ scrollY: window.pageYOffset });
     if (scrollY > 200) {
       document.getElementById('navigation-bar').style.backgroundColor = 'rgba(255, 255, 255, 0.75)';
     } else {
@@ -22,14 +29,16 @@ class App extends Component {
     }
   }
   componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+    this.setState({ loading: false })
   }
   componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll);
   }
   render() {
     return (
-      <div id={'App-container'}>
+      // {1 === 1 && <Header />}
+      this.state.loading ? <h1>loading</h1> : <div id={'App-container'}>
         <Header />
         <AboutMe />
         <Projects />
